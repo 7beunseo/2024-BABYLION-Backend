c2540740a73711b30b80720d6f147a017e761f1e
7beunseo <kimes0403@gmail.com>
2024-02-21 17:16:03 +0900

    🎉 프로젝트 생성

---
+++ src/main/java/likelion/applicant/util/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package likelion.applicant.util.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .build();
+    }
+
+}
